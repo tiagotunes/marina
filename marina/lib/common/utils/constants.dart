d47574418fcd2672b356bd65b00981bd88d36ec4
Tiago Antunes <tiago.antunes850@gmail.com>
2025-08-07 15:45:10 +0100

    Update Server API url

--- marina/lib/common/utils/constants.dart
+++ marina/lib/common/utils/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
   static const String SERVER_API_URL =
-      "https://marina-johk.onrender.com/api/v1";
+      "http://connectedthings-support.projects.fraunhofer.pt:3030/api/v1";
 
   static const String STORAGE_USER_SESSION_TOKEN = "user-session-token";
   static const String STORAGE_USER_PROFILE = "user-id";
