6825580e8e00f2fd4a12f464a174c3e8537ec47d
Tiago Antunes <tiago.antunes850@gmail.com>
2025-08-26 13:23:21 +0100

    Improve Profile page

--- marina/lib/features/profile/view/profile.dart
+++ marina/lib/features/profile/view/profile.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:marina/common/widgets/app_bar.dart';
+import 'package:marina/features/profile/view/widgets/profile_page.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: marinaAppBar(title: const Text("PERFIL")));
+    return Scaffold(
+      appBar: marinaAppBar(title: const Text("PERFIL")),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 24),
+        child: Column(children: [Expanded(child: profilePage())]),
+      ),
+    );
   }
 }
